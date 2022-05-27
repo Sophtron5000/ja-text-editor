6a5e4473e70141172f84944f1068f06da7e34386
Sophia C <95388964+Sophtron5000@user.noreply.github.com>
2022-05-26 21:22:35 -0700

    fix id in put method

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,11 +13,11 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => {
+export const putDb = async (id, content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.add({ id: autoIncrement, content });
+  const request = store.add({ id: id, content });
   const result = await request;
   console.log('data saved to the database', result);
 }
